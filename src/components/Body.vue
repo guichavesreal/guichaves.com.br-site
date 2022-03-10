9520a4b3fc985dc1f95a9d4c226c5f03684061e7
guilherme.chaves <guilherme.chaves@me.com.br>
2022-03-10 19:54:26 -0300

    Under construction

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="w-auto vh-100 d-flex justify-content-center align-items-center bd-highlight">
-        <h1>EM CONSTRUÇÃO</h1>
+        <h1>EM CONSTRUÇÃO...</h1>
         <!-- <img src="../assets/teste fundo transparente.png" alt="teste" class="image align-items-end justify-content-end"> -->
     </div>
 </template>
